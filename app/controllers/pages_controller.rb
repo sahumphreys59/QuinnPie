2963580839950dae2db03e64281fd1b9d977f17c
sahumphreys59 <sarah.humphreys59@gmail.com>
2020-04-28 18:43:40 -0700

    continue

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
 	def Home
 	end	
+
+	def About
+	end
+	
+	def Contact
+	end	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
+  get '/about' => 'pages#about'
+  get '/contact' => 'pages#contact'
 end
